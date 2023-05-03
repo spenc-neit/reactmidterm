952ba96165507d00446b680cda987551e0ef3726
spenc-neit <sdroucoulet@email.neit.edu>
2023-05-03 11:39:19 -0400

    added a single dollar sign and widened the box on the details page

--- src/ProductDetail.jsx
+++ src/ProductDetail.jsx
@@ -11,7 +11,7 @@ const ProductDetail = () =>{
 
     return(
         <>
-            <Box>
+            <Box detail>
                 <img src={`${entry.image}`} className="prodimg2" />
                 <div>
                     <Text color="navy" size="25px" link linkSrc={`${entry.id}`}>
@@ -20,7 +20,7 @@ const ProductDetail = () =>{
                     {/* <Text color="dimgray" size="20px">{entry.category} - {entry.rating.rate} stars ({entry.rating.count} ratings)</Text> */}
                         {/*I don't know what in the world is going with the line above but ProductDetail doesn't work at ALL until I comment it out and then un-comment it and then suddenly everything is fine. I'm gonna leave it commented out bc it isn't all that necessary but maybe you can tell me what's going on here, it makes no sense to me whatsoever*/ }
                     <Text color="navy" size="18px">{entry.description}</Text>
-                    <Text color="navy" size="20px">{entry.price}</Text> <br />
+                    <Text color="navy" size="20px">${entry.price}</Text> <br />
                     <Text color="blue" size="12px" link linkSrc="../products">Go back</Text>
                 </div>
             </Box>
